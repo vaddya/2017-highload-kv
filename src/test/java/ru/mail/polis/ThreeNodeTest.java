8bcf7b111333f1f9f2b52c7ab7a425c955e0edc1
Vadim Tsesko <incubos@yandex.com>
2017-11-06 18:17:40 +0300

    Help implementors with seconds-precision for conflict resolution

--- src/test/java/ru/mail/polis/ThreeNodeTest.java
+++ src/test/java/ru/mail/polis/ThreeNodeTest.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -23,7 +24,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class ThreeNodeTest extends ClusterTestBase {
     @Rule
-    public final Timeout globalTimeout = Timeout.seconds(3);
+    public final Timeout globalTimeout = Timeout.seconds(10);
     private int port0;
     private int port1;
     private int port2;
@@ -125,6 +126,9 @@ public class ThreeNodeTest extends ClusterTestBase {
         HttpResponse response = get(1, key, 2, 3);
         assertEquals(200, response.getStatusLine().getStatusCode());
         assertArrayEquals(value1, payloadOf(response));
+
+        // Help implementors with second precision for conflict resolution
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
 
         // Insert 2
         assertEquals(201, upsert(2, key, value2, 2, 3).getStatusLine().getStatusCode());
